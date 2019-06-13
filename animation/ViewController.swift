19a1f2d0c41c3781d79478c6b3ededa1d46aa4d2
chengxiaoyu <zacjjj@163.com>
2019-06-13 21:58:18 +0800

    Initial Commit

---
+++ animation/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  animation
+//
+//  Created by 成晓宇 on 6/13/19.
+//  Copyright © 2019 成晓宇. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
